1e4c1c29f6f205f1a6ebf68b27237c4d47a5365f
kain <zhangtqx@vip.qq.com>
2021-07-06 20:15:20 +0800

    update error code

--- bit.go
+++ bit.go
@@ -49,9 +49,9 @@ func (x *Bit) Crud(model interface{}, options ...CrudOption) *Crud {
 //	Params:
 //	 handlerFn: func(c *gin.Context) interface{}
 //	Mapped return:
-//	 (string) => 200 {"code":0,"msg":<this string value>}
-//	 (error) => 200 {"code":1,"msg":<this error msg, for example err.Error()>}
-//	 (default) => 200 {"code":0,"data":<interface{}>}
+//	 (string) => 200 {"error":0,"msg":<this string value>}
+//	 (error) => 200 {"error":1,"msg":<this error msg, for example err.Error()>}
+//	 (default) => 200 {"error":0,"data":<interface{}>}
 //	Custom error code: c.Set("code", 1000)
 func Bind(handlerFn interface{}) gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -59,8 +59,8 @@ func Bind(handlerFn interface{}) gin.HandlerFunc {
 			switch result := fn(c).(type) {
 			case string:
 				c.JSON(http.StatusOK, gin.H{
-					"code": 0,
-					"msg":  result,
+					"error": 0,
+					"msg":   result,
 				})
 				break
 			case error:
@@ -69,15 +69,15 @@ func Bind(handlerFn interface{}) gin.HandlerFunc {
 					code = 1
 				}
 				c.JSON(http.StatusOK, gin.H{
-					"code": code,
-					"msg":  result.Error(),
+					"error": code,
+					"msg":   result.Error(),
 				})
 				break
 			default:
 				if result != nil {
 					c.JSON(http.StatusOK, gin.H{
-						"code": 0,
-						"data": result,
+						"error": 0,
+						"data":  result,
 					})
 				} else {
 					c.Status(http.StatusNotFound)
