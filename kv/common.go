ede58ce2c876a707a6d2e0bd3c63eca059e84f87
kain <zhangtqx@vip.qq.com>
2023-02-24 08:54:16 +0800

    Fix: feishu oauth & ignore transfer/collector logs

--- kv/common.go
+++ kv/common.go
@@ -29,78 +29,80 @@ var DEFAULT = DynamicValues{
 }
 
 type DynamicValues struct {
-	// 会话周期（秒）
-	// 用户在 1 小时 内没有操作，将结束会话。
+	// session period (seconds)
+	// User inactivity for 1 hour, session will end
 	SessionTTL time.Duration `json:"session_ttl"`
-	// 登录锁定时间
-	// 锁定 15 分钟。
+	// login lockout time
+	// Locked for 15 minutes
 	LoginTTL time.Duration `json:"login_ttl"`
-	// 用户最大登录失败次数
-	// 有限时间（锁定时间）内连续登录失败 5 次，锁定帐号。
+	// Maximum number of login failures for a user
+	// If you fail to log in 5 times consecutively within a limited time (lockout time),
+	// your account will be locked
 	LoginFailures int64 `json:"login_failures"`
-	// IP 最大登录失败次数
-	// 同 IP 连续 10 次登录失败后，锁定 IP（周期为锁定时间）。
+	// Maximum number of login failures for the user's host IP
+	// If the same host IP fails to log in 10 times continuously, the IP will be locked (period is the login_ttl)
 	IpLoginFailures int64 `json:"ip_login_failures"`
-	// IP 白名单
-	// 白名单 IP 允许超出最大登录失败次数。
+	// IP whitelist
+	// Whitelisting IPs does not restrict login failure lockouts
 	IpWhitelist []string `json:"ip_whitelist"`
-	// GetIpBlacklist IP 黑名单
-	// 黑名单 IP 将禁止访问。
+	// IP blacklist
+	// will ban all access
 	IpBlacklist []string `json:"ip_blacklist"`
-	// 密码强度
-	// 0：无限制；
-	// 1：需要大小写字母；
-	// 2：需要大小写字母、数字；
-	// 3：需要大小写字母、数字、特殊字符
+	// password strength
+	// 0: unlimited
+	// 1: uppercase and lowercase letters
+	// 2: uppercase and lowercase letters, numbers
+	// 3: uppercase and lowercase letters, numbers, special characters
 	PwdStrategy int `json:"pwd_strategy"`
-	// 密码有效期（天）
-	// 密码过期后强制要求修改密码，0：永久有效
+	// password validity period
+	// After the password expires, it is mandatory to change the password, 0: permanently valid
 	PwdTTL time.Duration `json:"pwd_ttl"`
-	// 云平台
-	// tencent：腾讯云；
+	// Public Cloud
+	// Supported: Tencent Cloud `tencent`
+	// Plan: AWS `aws`, Alibaba Cloud `aliyun`
 	Cloud string `json:"cloud"`
-	// 腾讯云 API 密钥 Id
-	// 建议用子账号分配需要的权限
+	// Tencent Cloud API Secret Id
+	// It is recommended to use CAM to assign the required permissions
 	TencentSecretId string `json:"tencent_secret_id"`
-	// 腾讯云 API 密钥 Key
+	// Tencent Cloud API Secret Key
 	TencentSecretKey string `json:"tencent_secret_key,omitempty"`
-	// 腾讯云 COS 对象存储 Bucket（存储桶名称）
+	// Tencent Cloud COS bucket name
 	TencentCosBucket string `json:"tencent_cos_bucket,omitempty"`
-	// 腾讯云 COS 对象存储所属地域，例如：ap-guangzhou
+	// Tencent Cloud COS bucket region, for example: ap-guangzhou
 	TencentCosRegion string `json:"tencent_cos_region"`
-	// 腾讯云 COS 对象存储预签名有效期，单位：秒
+	// Tencent Cloud COS bucket pre-signature validity period, unit: second
 	TencentCosExpired int `json:"tencent_cos_expired"`
-	// 腾讯云 COS 对象存储上传大小限制，单位：KB
+	// Tencent Cloud COS bucket upload size limit, unit: KB
 	TencentCosLimit int64 `json:"tencent_cos_limit"`
-	// 办公平台
-	// feishu：飞书；
-	Office string `json:"office"`
-	// 飞书应用 ID
+	// Enterprise Collaboration
+	// Supported: feishu, this is an integrated development experiment, Feishu and Lark applications are not universal
+	// Office string `json:"office"`
+	// Feishu App ID
 	FeishuAppId string `json:"feishu_app_id"`
-	// 飞书应用密钥
+	// Feishu application key
 	FeishuAppSecret string `json:"feishu_app_secret,omitempty"`
-	// 飞书事件订阅安全校验数据密钥
+	// Feishu event subscription security verification data key
 	FeishuEncryptKey string `json:"feishu_encrypt_key,omitempty"`
-	// 飞书事件订阅验证令牌
+	// Feishu Event Subscription Verification Token
 	FeishuVerificationToken string `json:"feishu_verification_token,omitempty"`
-	// 第三方免登授权码跳转地址
+	// Third-party registration-free authorization code redirection address
 	RedirectUrl string `json:"redirect_url"`
-	// 公共电子邮件服务 SMTP 地址
+	// Public email service SMTP address
 	EmailHost string `json:"email_host"`
-	// SMTP 端口号（SSL）
+	// Public email SMTP port number (SSL)
 	EmailPort string `json:"email_port"`
-	// 公共邮箱用户
+	// Public email username
 	EmailUsername string `json:"email_username"`
-	// 公共邮箱用户
+	// Public email password
 	EmailPassword string `json:"email_password,omitempty"`
-	// 开放服务地址
+	// Openapi url
 	OpenapiUrl string `json:"openapi_url"`
-	// 开放服务应用认证 Key
-	// API 网关应用认证方式 https://cloud.tencent.com/document/product/628/55088
+	// Openapi application authentication key
+	// API gateway application authentication https://cloud.tencent.com/document/product/628/55088
 	OpenapiKey string `json:"openapi_key"`
-	// 开放服务应用认证密钥
+	// Openapi Application Authentication Secret
 	OpenapiSecret string `json:"openapi_secret,omitempty"`
-	// DSL 集合控制
+	// DSL Control Variables
 	DSL map[string]*DSLOption `json:"dsl,omitempty"`
 }
 
