f8266ec0a212ab27421c1162bb39472249695388
kain <zhangtqx@vip.qq.com>
2023-07-06 16:28:38 +0800

    update: simplify passlib

---
+++ passlib/passlib.go
@@ -0,0 +1,79 @@
+package passlib
+
+import (
+	"crypto/rand"
+	"crypto/subtle"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"golang.org/x/crypto/argon2"
+	"strings"
+)
+
+var (
+	DEFAULT_MEMORY_COST uint32 = 65536
+	DEFAULT_TIME_COST   uint32 = 4
+	DEFAULT_THREADS     uint8  = 1
+)
+
+var (
+	ErrInvalidHash         = errors.New("unable to parse the current hash value")
+	ErrIncompatibleVariant = errors.New("hash variants are not compatible")
+	ErrIncompatibleVersion = errors.New("hash version are not support")
+	ErrNotMatch            = errors.New("password does not match hash")
+)
+
+func Hash(password string) (hash string, err error) {
+	salt := make([]byte, 16)
+	if _, err = rand.Read(salt); err != nil {
+		return
+	}
+
+	key := argon2.IDKey([]byte(password), salt,
+		DEFAULT_TIME_COST, DEFAULT_MEMORY_COST, DEFAULT_THREADS, 32)
+
+	return fmt.Sprintf(`$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s`,
+		argon2.Version, DEFAULT_MEMORY_COST, DEFAULT_TIME_COST, DEFAULT_THREADS,
+		base64.RawStdEncoding.EncodeToString(salt),
+		base64.RawStdEncoding.EncodeToString(key),
+	), nil
+}
+
+func Verify(password string, hash string) (err error) {
+	options := strings.Split(hash, "$")
+	if len(options) != 6 {
+		return ErrInvalidHash
+	}
+	if options[1] != "argon2id" {
+		return ErrIncompatibleVariant
+	}
+	var version int
+	if _, err = fmt.Sscanf(options[2], "v=%d", &version); err != nil {
+		return ErrIncompatibleVersion
+	}
+	if version != argon2.Version {
+		return ErrIncompatibleVersion
+	}
+	var memory uint32
+	var time uint32
+	var threads uint8
+	if _, err = fmt.Sscanf(options[3], "m=%d,t=%d,p=%d", &memory, &time, &threads); err != nil {
+		return ErrInvalidHash
+	}
+	var salt []byte
+	if salt, err = base64.RawStdEncoding.Strict().DecodeString(options[4]); err != nil {
+		return
+	}
+	var key []byte
+	if key, err = base64.RawStdEncoding.Strict().DecodeString(options[5]); err != nil {
+		return
+	}
+	otherKey := argon2.IDKey([]byte(password), salt, time, memory, threads, uint32(len(key)))
+	if subtle.ConstantTimeEq(int32(len(key)), int32(len(otherKey))) == 0 {
+		return ErrNotMatch
+	}
+	if subtle.ConstantTimeCompare(key, otherKey) == 1 {
+		return
+	}
+	return ErrNotMatch
+}
---
+++ passlib/passlib_test.go
@@ -0,0 +1,35 @@
+package passlib_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/weplanx/go-wpx/passlib"
+	"testing"
+)
+
+func TestHashAndVerify(t *testing.T) {
+	hash, err := passlib.Hash("pass@VAN1234")
+	assert.NoError(t, err)
+	err = passlib.Verify("pass@VAN1234", hash)
+	assert.NoError(t, err)
+	err = passlib.Verify("pass@VAN1235", hash)
+	assert.ErrorIs(t, err, passlib.ErrNotMatch)
+}
+
+const PASS1 = `$argon2i$v=19$m=65536,t=4,p=1$NPCjKIcoU2z6rg6p8glOfg$jrbRcvsTq/ITJP414/xhNNwOtVeHYa478hPn8M6uJLA`
+const PASS2 = `$argon2id$v=x$m=65536,t=4,p=1$NPCjKIcoU2z6rg6p8glOfg$jrbRcvsTq/ITJP414/xhNNwOtVeHYa478hPn8M6uJLA`
+const PASS3 = `$argon2id$v=18$m=65536,t=4,p=1$NPCjKIcoU2z6rg6p8glOfg$jrbRcvsTq/ITJP414/xhNNwOtVeHYa478hPn8M6uJLA`
+const PASS4 = `$argon2id$v=19$xcxcsdsdwe$NPCjKIcoU2z6rg6p8glOfg$jrbRcvsTq/ITJP414/xhNNwOtVeHYa478hPn8M6uJLA`
+
+func TestVerifyErrors(t *testing.T) {
+	var err error
+	err = passlib.Verify("pass@VAN1234", "asdaqweqwexcxzcqweqw")
+	assert.ErrorIs(t, err, passlib.ErrInvalidHash)
+	err = passlib.Verify("pass@VAN1234", PASS1)
+	assert.ErrorIs(t, err, passlib.ErrIncompatibleVariant)
+	err = passlib.Verify("pass@VAN1234", PASS2)
+	assert.ErrorIs(t, err, passlib.ErrIncompatibleVersion)
+	err = passlib.Verify("pass@VAN1234", PASS3)
+	assert.ErrorIs(t, err, passlib.ErrIncompatibleVersion)
+	err = passlib.Verify("pass@VAN1234", PASS4)
+	assert.ErrorIs(t, err, passlib.ErrInvalidHash)
+}
